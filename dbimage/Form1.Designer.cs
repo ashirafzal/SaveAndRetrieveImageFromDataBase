66bb3921427cfac9dab214447bbd9a925821af10
ashirafzal <ashirafzal96@gmail.com>
2019-03-30 19:26:30 -0700

    Add project files.

---
+++ dbimage/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace dbimage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.View = new System.Windows.Forms.Button();
+            this.Browse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insert image into sql database";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // textID
+            // 
+            this.textID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textID.Location = new System.Drawing.Point(166, 154);
+            this.textID.Multiline = true;
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(226, 29);
+            this.textID.TabIndex = 3;
+            // 
+            // textName
+            // 
+            this.textName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textName.Location = new System.Drawing.Point(166, 207);
+            this.textName.Multiline = true;
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(226, 29);
+            this.textName.TabIndex = 4;
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.SystemColors.WindowText;
+            this.Save.FlatAppearance.BorderSize = 3;
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.ForeColor = System.Drawing.Color.White;
+            this.Save.Location = new System.Drawing.Point(98, 279);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(136, 46);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // View
+            // 
+            this.View.BackColor = System.Drawing.SystemColors.WindowText;
+            this.View.FlatAppearance.BorderSize = 3;
+            this.View.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.View.ForeColor = System.Drawing.Color.White;
+            this.View.Location = new System.Drawing.Point(267, 279);
+            this.View.Name = "View";
+            this.View.Size = new System.Drawing.Size(136, 46);
+            this.View.TabIndex = 6;
+            this.View.Text = "View";
+            this.View.UseVisualStyleBackColor = false;
+            this.View.Click += new System.EventHandler(this.View_Click);
+            // 
+            // Browse
+            // 
+            this.Browse.BackColor = System.Drawing.SystemColors.WindowText;
+            this.Browse.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Browse.FlatAppearance.BorderSize = 3;
+            this.Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Browse.ForeColor = System.Drawing.Color.White;
+            this.Browse.Location = new System.Drawing.Point(543, 330);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(136, 46);
+            this.Browse.TabIndex = 7;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = false;
+            this.Browse.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(484, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 219);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(766, 422);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.View);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button View;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ dbimage/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace dbimage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-9CBGPDG\ASHIRAFZAL;Initial Catalog=PracticeDB;Integrated Security=True;Pooling=False");
+
+        string imgLocation = "";
+        SqlCommand cmd;
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Filter = "png files(*.png)|*.png|jpg files(*.jpg)|*.jpg All files(*.*)|*.* ";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imgLocation = dialog.FileName.ToString();
+                pictureBox1.ImageLocation = imgLocation;
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            byte[] images = null;
+            FileStream Stream = new FileStream(imgLocation, FileMode.Open,FileAccess.Read);
+            BinaryReader brs = new BinaryReader(Stream);
+            images = brs.ReadBytes((int)Stream.Length);
+
+            connection.Open();
+            int a = Convert.ToInt32(textID.Text);
+            string sqlQuery = "insert into BioData(ID,Name,Image) VALUES ('"+ a + "' , '" + textName.Text + "' ,@images)";
+            cmd = new SqlCommand(sqlQuery,connection);
+            cmd.Parameters.Add(new SqlParameter("@images", images));
+            int N = cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show(N.ToString()+"Data Saved Successfully");
+            
+        }
+
+        private void View_Click(object sender, EventArgs e)
+        {
+         
+
+            connection.Open();
+            string sqlQuery = "select Name,Image from BioData where id = '" + textID.Text + "'";
+            cmd = new SqlCommand(sqlQuery,connection);
+            SqlDataReader DataRead = cmd.ExecuteReader();
+            DataRead.Read();
+            
+
+            if (DataRead.HasRows)
+            {
+                textName.Text = DataRead[0].ToString();
+                byte[] images = ((byte[])DataRead[1]);
+
+                if (images == null)
+                {
+                    pictureBox1.Image = null;
+                }
+                else
+                {
+                    MemoryStream mstream = new MemoryStream(images);
+                    pictureBox1.Image = Image.FromStream(mstream);
+
+                }
+                connection.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("This Data is Not Available");
+            }
+
+            if (cmd.Connection.State == ConnectionState.Open)
+            {
+                cmd.Connection.Close();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+        }
+    }
+}
